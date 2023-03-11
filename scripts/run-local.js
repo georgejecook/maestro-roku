c2a0f4e6533b96fe9089f51dd8d5167fcfeadccf
George Cook <george@tantawowa.com>
2023-03-11 20:20:31 +0100

    adds tdd test support for rapid testing

--- scripts/run-local.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const ProgramBuilder = require('brighterscript').ProgramBuilder;
-
-let programBuilder = new ProgramBuilder();
-programBuilder.run({
-  project: path.join(__dirname, '../', 'bsconfig-local.json')
-}).catch(e => {
-  console.error(e);
-});
