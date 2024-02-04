8b230e6096774d5467d267b19cb852f7d87be16f
georgejecook <george@tantawowa.com>
2024-02-04 21:49:47 +0000

    adds maps

---
+++ game-assets/gameart2d-desert.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="gameart2d-desert" tilewidth="128" tileheight="128" tilecount="40" columns="5">
+ <image source="gameart2d-desert.png" width="640" height="1024"/>
+ <tile id="2">
+  <properties>
+   <property name="type" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="type" type="int" value="1"/>
+  </properties>
+ </tile>
+</tileset>
